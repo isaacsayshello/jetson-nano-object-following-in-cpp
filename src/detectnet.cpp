28c05bfd4b27d582d189d2d22aab7585a830d833
isaacsayshello <isaacsayshello@protonmail.com>
2021-05-16 13:01:00 -0500

    cleaned up the main function

--- src/detectnet.cpp
+++ src/detectnet.cpp
@@ -1,133 +1,97 @@
+//THIS FILE CONTAINS THE MAIN FUNC AND IS PROBABLY WHERE YOU WOULD ADD YOUR OWN CODE
 
 #include <signal.h>
 #include <iostream>
-
 #include "jetson-utils/videoSource.h"
 #include "jetson-utils/videoOutput.h"
-
 #include "jetson-inference/detectNet.h"
-
 #include "followBottle/followBottle.h" 
-
 #ifdef HEADLESS
 	#define IS_HEADLESS() "headless"	// run without display
 #else
 	#define IS_HEADLESS() (const char*)NULL
 #endif
-
 #include "everyThingIDontWantToDealWith.h"
-
 
 int main( int argc, char** argv )
 {
-	/*
-	 * parse command line
-	 */
+	//parse command line
 	commandLine cmdLine(argc, argv, IS_HEADLESS());
-
 	if( cmdLine.GetFlag("help") )
 		return usage();
-
-
-	/*
-	 * attach signal handler
-	 */
+	//attach signal handler
 	if( signal(SIGINT, sig_handler) == SIG_ERR )
 		LogError("can't catch SIGINT\n");
-
-
-	/*
-	 * create input stream
-	 */
+	//create input stream
 	videoSource* input = videoSource::Create(cmdLine, ARG_POSITION(0));
-
 	if( !input )
 	{
 		LogError("detectnet:  failed to create input stream\n");
 		return 0;
 	}
-
-
-	/*
-	 * create output stream
-	 */
+	//create output stream
 	videoOutput* output = videoOutput::Create(cmdLine, ARG_POSITION(1));
-	
 	if( !output )
 		LogError("detectnet:  failed to create output stream\n");	
-
-//***************************************DETECT STUFF***************	
-
-	/*
-	 * create detection network
-	 */
+	//create detection network
 	detectNet* net = detectNet::Create(cmdLine);
-	
 	if( !net )
 	{
 		LogError("detectnet:  failed to load detectNet model\n");
 		return 0;
 	}
-
 	// parse overlay flags
 	const uint32_t overlayFlags = detectNet::OverlayFlagsFromStr(cmdLine.GetString("overlay", "box,labels,conf"));
+
 	//Here is were i want to create followbottle class so 
 	//that i can pass net into it
-		
 	//creation of followBottle class
 	FollowBottle followBottle{net}; 
+	followBottle.initI2C(); //this sets up the i2c to arduino	
 
-	followBottle.initI2C();	
-
-	/*
-	 * processing loop
-	 */
+	//*********************processing loop*********
 	while( !signal_recieved )
 	{
 		// capture next image image
 		uchar3* image = NULL;
-
 		if( !input->Capture(&image, 1000) )
 		{
 			// check for EOS
 			if( !input->IsStreaming() )
 				break; 
-
 			LogError("detectnet:  failed to capture video frame\n");
 			continue;
 		}
-
 		// detect objects in the frame
 		detectNet::Detection* detections = NULL;
-	
 		const int numDetections = net->Detect(image, input->GetWidth(), input->GetHeight(), &detections, overlayFlags);
-		
 		if( numDetections > 0 )
 		{
 			LogVerbose("%i objects detected\n", numDetections);
-		
 			for( int n=0; n < numDetections; n++ )
 			{
-				LogVerbose("detected obj %i  class #%u (%s)  confidence=%f\n", n, detections[n].ClassID, net->GetClassDesc(detections[n].ClassID), detections[n].Confidence);
-				LogVerbose("bounding box %i  (%f, %f)  (%f, %f)  w=%f  h=%f\n", n, detections[n].Left, detections[n].Top, detections[n].Right, detections[n].Bottom, detections[n].Width(), detections[n].Height()); 
+				LogVerbose("detected obj %i  class #%u (%s)  confidence=%f\n", 
+					n, detections[n].ClassID, net->GetClassDesc(detections[n].ClassID), 
+					detections[n].Confidence);
+				LogVerbose("bounding box %i  (%f, %f)  (%f, %f)  w=%f  h=%f\n", 
+					n, detections[n].Left, detections[n].Top, detections[n].Right, 
+					detections[n].Bottom, detections[n].Width(), detections[n].Height()); 
 			}
 		}	
-
 		// render outputs
 		if( output != NULL )
 		{
 			output->Render(image, input->GetWidth(), input->GetHeight());
-
 			// update the status bar
 			char str[256];
-			sprintf(str, "TensorRT %i.%i.%i | %s | Network %.0f FPS", NV_TENSORRT_MAJOR, NV_TENSORRT_MINOR, NV_TENSORRT_PATCH, precisionTypeToStr(net->GetPrecision()), net->GetNetworkFPS());
+			sprintf(str, "TensorRT %i.%i.%i | %s | Network %.0f FPS", 
+				NV_TENSORRT_MAJOR, NV_TENSORRT_MINOR, NV_TENSORRT_PATCH, 
+				precisionTypeToStr(net->GetPrecision()), net->GetNetworkFPS());
 			output->SetStatus(str);
-
 			// check if the user quit
 			if( !output->IsStreaming() )
 				signal_recieved = true;
 		}
-
 		// print out timing info
 		net->PrintProfilerTimes();
 		//--------------------------------------------------------------my stuff----------------------
@@ -136,19 +100,14 @@ int main( int argc, char** argv )
 		followBottle.findClosest(detections, numDetections); 
 		followBottle.driveRobot();
 		//followBottle.skidRight(1);
-
 	}
 	
 
-	/*
-	 * destroy resources
-	 */
+	//destroy resources
 	LogVerbose("detectnet:  shutting down...\n");
-	
 	SAFE_DELETE(input);
 	SAFE_DELETE(output);
 	SAFE_DELETE(net);
-
 	LogVerbose("detectnet:  shutdown complete.\n");
 	return 0;
 }
